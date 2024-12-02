182dd6e9a5bfd0c71f0f6ac4662bfa8453ffe2fa
ishay320 <ishay320@hotmail.com>
2024-12-02 15:06:35 +0200

    feat(array): implemented reserve and resize, and some cleanup

--- array/array.h
+++ array/array.h
@@ -37,94 +37,6 @@ struct array {
 
 #define ARRAY_HEADER_SIZE (sizeof(size_t) * 3)
 
-void* f_array_init(size_t stride, size_t cap)
-{
-    struct array* header = malloc(ARRAY_HEADER_SIZE + (stride * cap));
-    if (!header) {
-        perror("could not create dynamic array");
-        return NULL;
-    }
-
-    header->cap    = cap;
-    header->len    = 0;
-    header->stride = stride;
-
-    return header->data;
-}
-
-bool f_array_push(void** array, void* element)
-{
-    assert(*array);
-    struct array* header = (*array) - ARRAY_HEADER_SIZE;
-    if (header->len == header->cap) {
-        size_t new_cap = header->cap * 2;
-        void* new_ptr =
-            realloc(header, new_cap * header->stride + ARRAY_HEADER_SIZE);
-        if (!new_ptr) {
-            perror("could not create dynamic array");
-            return false;
-        }
-        header      = new_ptr;
-        header->cap = new_cap;
-
-        // update the outside ptr to the new position
-        *array = header->data;
-    }
-
-    void* insert_pos = header->data + (header->stride * header->len);
-    memcpy(insert_pos, element, header->stride);
-    header->len++;
-
-    return true;
-}
-
-void f_array_free(void** array)
-{
-    assert(*array);
-    struct array* header = (*array) - ARRAY_HEADER_SIZE;
-    free(header);
-    *array = NULL;
-}
-
-/**
- * Get the capacity of the array.
- *
- * @param array pointer to the array.
- * @return The capacity of the array.
- */
-size_t array_cap(void* array)
-{
-    assert(array);
-    struct array* header = array - ARRAY_HEADER_SIZE;
-    return header->cap;
-}
-
-/**
- * Get the length of the array.
- *
- * @param array pointer to the array.
- * @return The number of elements currently in the array.
- */
-size_t array_len(void* array)
-{
-    assert(array);
-    struct array* header = array - ARRAY_HEADER_SIZE;
-    return header->len;
-}
-
-/**
- * Get the stride (size of each element) of the dynamic array.
- *
- * @param array pointer to the dynamic array.
- * @return The size in bytes of each element in the array.
- */
-size_t array_stride(void* array)
-{
-    assert(array);
-    struct array* header = array - ARRAY_HEADER_SIZE;
-    return header->stride;
-}
-
 /**
  * Initialisation of the dynamic array
  *
@@ -147,7 +59,7 @@ size_t array_stride(void* array)
  *
  * @param array is the array to free.
  */
-#define array_free(array) f_array_free((void**)&array)
+void array_free(void* array);
 
 /**
  * Push an element to the array
@@ -171,5 +83,144 @@ size_t array_stride(void* array)
         typeof(element) __tmp_value = (element); \
         array_push((array), __tmp_value);        \
     })
+
+/**
+ * Get the header from the array pointer
+ *
+ * @param a is the array.
+ * @return header of the array
+ */
+#define array_get_header(a) (void*)(a) - ARRAY_HEADER_SIZE;
+
+/**
+ * Reserves space in the array, if the array is bigger then the reserve size, it
+ * wont do nothing
+ *
+ * @param array is the array to reserve.
+ * @param reserve is the size to reserve.
+ * @return bool of if reserved successfully.
+ */
+#define array_reserve(array, reserve) f_array_reserve((void**)(&array), reserve)
+
+/**
+ * Resize space in the array, it can make the array smaller or bigger
+ *
+ * @param array is the array to reserve.
+ * @param resize is the size to resize to.
+ * @return bool of if resized successfully.
+ */
+#define array_resize(array, resize) f_array_resize((void**)(&array), resize)
+
+void* f_array_init(size_t stride, size_t cap)
+{
+    struct array* header = malloc(ARRAY_HEADER_SIZE + (stride * cap));
+    if (!header) {
+        perror("could not create dynamic array");
+        return NULL;
+    }
+
+    header->cap    = cap;
+    header->len    = 0;
+    header->stride = stride;
+
+    return header->data;
+}
+
+void array_free(void* array)
+{
+    assert(array);
+    struct array* header = array_get_header(array);
+    free(header);
+}
+
+bool f_array_resize(void** array, size_t size)
+{
+    assert(*array);
+    struct array* header = array_get_header(*array);
+    void* new_ptr = realloc(header, size * header->stride + ARRAY_HEADER_SIZE);
+    if (!new_ptr) {
+        perror("could not resize array");
+        return false;
+    }
+
+    header      = new_ptr;
+    header->cap = size;
+    if (header->cap < header->len) {
+        header->len = header->cap;
+    }
+
+    // update the outside ptr to the new position
+    *array = header->data;
+    return true;
+}
+
+bool f_array_reserve(void** array, size_t size)
+{
+    assert(*array);
+    struct array* header = array_get_header(*array);
+    if (header->cap >= size) {
+        return true;
+    }
+
+    return f_array_resize(array, size);
+}
+
+bool f_array_push(void** array, void* element)
+{
+    assert(*array);
+    struct array* header = array_get_header(*array);
+    if (header->len == header->cap) {
+        size_t new_cap = header->cap * 2;
+        if (!f_array_resize(array, new_cap)) {
+            return false;
+        }
+        header = array_get_header(*array);
+    }
+
+    void* insert_pos = header->data + (header->stride * header->len);
+    memcpy(insert_pos, element, header->stride);
+    header->len++;
+
+    return true;
+}
+
+/**
+ * Get the capacity of the array.
+ *
+ * @param array pointer to the array.
+ * @return The capacity of the array.
+ */
+size_t array_cap(void* array)
+{
+    assert(array);
+    struct array* header = array_get_header(array);
+    return header->cap;
+}
+
+/**
+ * Get the length of the array.
+ *
+ * @param array pointer to the array.
+ * @return The number of elements currently in the array.
+ */
+size_t array_len(void* array)
+{
+    assert(array);
+    struct array* header = array_get_header(array);
+    return header->len;
+}
+
+/**
+ * Get the stride (size of each element) of the dynamic array.
+ *
+ * @param array pointer to the dynamic array.
+ * @return The size in bytes of each element in the array.
+ */
+size_t array_stride(void* array)
+{
+    assert(array);
+    struct array* header = array_get_header(array);
+    return header->stride;
+}
 
 #endif  // ARRAY_H
--- array/test.c
+++ array/test.c
@@ -6,7 +6,7 @@
 void test_array_initialization()
 {
     int* arr = array_init(int);
-    assert(arr != NULL);  // Verify array is initialized
+    assert(arr != NULL);  // verify array is initialized
     array_free(arr);
     printf("Initialization test passed.\n");
 }
@@ -14,8 +14,8 @@ void test_array_initialization()
 void test_array_init_sized()
 {
     int* arr = array_init_sized(int, 10);
-    assert(arr != NULL);  // Verify array is initialized
-    struct array* header = (struct array*)(((uint8_t*)arr) - ARRAY_HEADER_SIZE);
+    assert(arr != NULL);  // verify array is initialized
+    struct array* header = array_get_header(arr);
     assert(header->cap == 10);
     array_free(arr);
     printf("Initialization with size test passed.\n");
@@ -25,7 +25,7 @@ void test_array_push_rval()
 {
     int* arr = array_init(int);
     array_push_rval(arr, 42);
-    struct array* header = (struct array*)(((uint8_t*)arr) - ARRAY_HEADER_SIZE);
+    struct array* header = array_get_header(arr);
     assert(header->len == 1);
     assert(arr[0] == 42);
     array_free(arr);
@@ -37,7 +37,7 @@ void test_array_push()
     int* arr = array_init(int);
     int item = 42;
     array_push(arr, item);
-    struct array* header = (struct array*)(((uint8_t*)arr) - ARRAY_HEADER_SIZE);
+    struct array* header = array_get_header(arr);
     assert(header->len == 1);
     assert(arr[0] == 42);
     array_free(arr);
@@ -50,7 +50,7 @@ void test_array_resizing_dynamicly()
     for (int i = 0; i < 100; i++) {
         array_push(arr, i);
     }
-    struct array* header = (struct array*)(((uint8_t*)arr) - ARRAY_HEADER_SIZE);
+    struct array* header = array_get_header(arr);
     assert(header->cap >= 100);
     assert(header->len == 100);
     for (int i = 0; i < 100; i++) {
@@ -65,14 +65,13 @@ void test_array_free()
     int* arr = array_init(int);
     array_push_rval(arr, 10);
     array_free(arr);
-    assert(arr == NULL);  // Verify array pointer is null after free
     printf("Free test passed.\n");
 }
 
 void test_array_cap()
 {
     int* arr             = array_init_sized(int, 50);
-    struct array* header = (struct array*)(((uint8_t*)arr) - ARRAY_HEADER_SIZE);
+    struct array* header = array_get_header(arr);
     assert(array_cap(arr) == header->cap);
     assert(header->cap == 50);
     array_free(arr);
@@ -82,13 +81,13 @@ void test_array_cap()
 void test_array_len()
 {
     int* arr = array_init(int);
-    assert(array_len(arr) == 0);  // Initially, the length should be 0.
+    assert(array_len(arr) == 0);  // initially, the length should be 0
 
     array_push_rval(arr, 42);
-    assert(array_len(arr) == 1);  // After one push, the length should be 1.
+    assert(array_len(arr) == 1);  // after one push, the length should be 1
 
     array_push_rval(arr, 100);
-    assert(array_len(arr) == 2);  // After two pushes, the length should be 2.
+    assert(array_len(arr) == 2);  // after two pushes, the length should be 2
 
     array_free(arr);
     printf("Length test passed.\n");
@@ -98,17 +97,44 @@ void test_array_stride()
 {
     int* int_arr = array_init(int);
     assert(array_stride(int_arr) ==
-           sizeof(int));  // Stride for int array should match sizeof(int).
+           sizeof(int));  // stride for int array should match sizeof(int)
 
     double* double_arr = array_init(double);
     assert(
         array_stride(double_arr) ==
-        sizeof(
-            double));  // Stride for double array should match sizeof(double).
+        sizeof(double));  // stride for double array should match sizeof(double)
 
     array_free(int_arr);
     array_free(double_arr);
     printf("Stride test passed.\n");
+}
+
+void test_array_reserve()
+{
+    int* array = array_init(int);
+    assert(array_reserve(array, 10));
+    assert(array_cap(array) == 10);
+    assert(array_reserve(array, 5));
+    assert(array_cap(array) == 10);
+    assert(array_reserve(array, 30));
+    assert(array_cap(array) == 30);
+
+    array_free(array);
+    printf("Reserve test passed.\n");
+}
+
+void test_array_resize()
+{
+    int* array = array_init(int);
+    assert(array_resize(array, 10));
+    assert(array_cap(array) == 10);
+    assert(array_resize(array, 7));
+    assert(array_cap(array) == 7);
+    assert(array_resize(array, 30));
+    assert(array_cap(array) == 30);
+
+    array_free(array);
+    printf("Resize test passed.\n");
 }
 
 int main(void)
@@ -122,6 +148,8 @@ int main(void)
     test_array_cap();
     test_array_len();
     test_array_stride();
+    test_array_resize();
+    test_array_reserve();
     printf("All tests passed!\n");
     return 0;
 }
